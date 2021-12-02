04046883be190e4d475c4187a5be47e9362f7d68
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2021-12-02 12:56:12 +0100

    prepare Day 3

---
+++ src/Day03.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+// test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
