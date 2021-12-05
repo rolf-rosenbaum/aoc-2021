7ffe86a293bfd817a7bc59572612e48850922426
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2021-12-05 07:27:27 +0100

    Day 5 preparation

---
+++ src/Day05.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    fun part1(input: Input): Int {
+        return 0
+    }
+    
+    fun part2(input: Input): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day05_test")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day05")
+    println(part1(input))
+    println(part2(input))
+}
