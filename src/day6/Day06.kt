aec38b0bf4754b0f96e817ef9eb7802598085ac4
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2021-12-06 12:51:24 +0100

    Day 6 solution

--- src/day6/Day06.kt
+++ src/day6/Day06.kt
@@ -4,23 +4,46 @@ import Input
 import readInput
 
 fun main() {
-    
-    fun part1(input: Input): Int {
-        return 0
+    fun part1(input: Input): Long {
+
+        return input.first().split(",").map(String::toInt)
+            .groupBy { it }
+            .mapValues { it.value.count().toLong() }
+            .toMutableMap()
+            .countAfterDays(80)
     }
 
-    fun part2(input: Input): Int {
-        return 0
-            
+    fun part2(input: Input): Long {
+        return input.first().split(",").map(String::toInt)
+            .groupBy { it }
+            .mapValues { it.value.count().toLong() }
+            .toMutableMap()
+            .countAfterDays(256)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("template/test_input")
-    val input = readInput("template/input")
+    val testInput = readInput("day6/test_input")
+    val input = readInput("day6/input")
 
-    check(part1(testInput) == 0)
+    val part1 = part1(testInput)
+    println(part1)
+    check(part1 == 5934L)
     println(part1(input))
 
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 26984457539L)
     println(part2(input))
+}
+
+fun MutableMap<Int, Long>.countAfterDays(days: Int): Long {
+    val swarm = this
+    repeat(days) {
+        val foo = swarm[0] ?: 0
+        (0..5).map { n ->
+            swarm[n] = swarm[n+1] ?: 0
+        }
+        swarm[6] = (swarm[7] ?: 0) + foo
+        swarm[7] = swarm[8] ?: 0
+        swarm[8] = foo
+    }
+    return swarm.values.sum()
 }
